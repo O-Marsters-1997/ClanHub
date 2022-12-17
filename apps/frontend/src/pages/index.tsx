486df950772e905e06bd746dcea9ad0f6aa78984
Olly Marsters <ollymarsters@yahoo.co.uk>
2022-12-17 12:23:41 +0000

    reset prettier config to run on all src files

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -3,14 +3,10 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 
-
-
-
-
-
 const inter = Inter({ subsets: ["latin"] });
 
 // sample change
+
 export default function Home() {
   return (
     <>
